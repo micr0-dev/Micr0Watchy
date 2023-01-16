10ac4115f8655d98ccbfc0ebfbb50e1bee9d3400
Micr0Arch <miraslauk@gmail.com>
2023-01-15 19:09:35 -0500

    Added Graceful shutdown

--- Micr0WatchyServer.py
+++ Micr0WatchyServer.py
@@ -3,7 +3,7 @@ import spotipy, pyowm
 # Networking
 import http.server, socketserver, requests, json, ssl
 # Other
-import time, threading, os, dotenv
+import time, threading, os, dotenv, signal, sys
 
 # Load .env
 dotenv.load_dotenv()
@@ -33,6 +33,9 @@ redirect_uri = "http://"+os.getenv('HOST_IP')+":18723/"
 # Get path of project directory
 path = os.path.dirname(__file__)+"/"
 
+isShutingDown = False
+runningServiceCount = 0
+
 # --- Redirect Server for spotify authorization ---
 
 def redirectServer():
@@ -43,9 +46,11 @@ def redirectServer():
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print("serving at port", PORT, end="")
         print("... Success!")
+        runningServiceCount+=1
         time.sleep(60*2)
         print("Shuting Down Redirect Server... ", end="")
     print("Success!")
+    runningServiceCount-=1
 
 
 print("Starting Redirect Server, ", end="")
@@ -104,35 +109,43 @@ def tokenloop():
     global sp, access_token
     reftime = 0
     time.sleep(1)
-    while True:
-        print("Refreshing Token...", end="")
-        try:
-            token_info = oauth.get_cached_token()
-            reftime = token_info["expires_at"]-int(time.time())-200
-            refresh_token = token_info["refresh_token"]
-            oauth.refresh_access_token(refresh_token)
-            access_token = oauth.get_cached_token()["access_token"]
-            sp = spotipy.Spotify(auth=access_token)
-        except requests.exceptions.ReadTimeout:
-            print("Timed Out...")
-            infoDict["sptresponsecode"] = 504
-        except spotipy.client.SpotifyException:
-            print("SpotifyException, Most likely expired token")
-            infoDict["sptresponsecode"] = 502
-        except requests.exceptions.ConnectionError:
-            print("ConnectionError")
-            infoDict["sptresponsecode"] = 503
-        except Exception as e:
-            infoDict["sptresponsecode"] = 500
-            print(e)
-        infoDict["spiresponsecode"] = 200
-        print(" Success! Next refresh in "+str(reftime)+" seconds.")
-        time.sleep(reftime)
+    runningServiceCount+=1
+    loopCount = 0
+    while (not isShutingDown):
+        if loopCount >= reftime:
+            print("Refreshing Token...", end="")
+            try:
+                token_info = oauth.get_cached_token()
+                reftime = token_info["expires_at"]-int(time.time())-200
+                refresh_token = token_info["refresh_token"]
+                oauth.refresh_access_token(refresh_token)
+                access_token = oauth.get_cached_token()["access_token"]
+                sp = spotipy.Spotify(auth=access_token)
+            except requests.exceptions.ReadTimeout:
+                print("Timed Out...")
+                infoDict["sptresponsecode"] = 504
+            except spotipy.client.SpotifyException:
+                print("SpotifyException, Most likely expired token")
+                infoDict["sptresponsecode"] = 502
+            except requests.exceptions.ConnectionError:
+                print("ConnectionError")
+                infoDict["sptresponsecode"] = 503
+            except Exception as e:
+                infoDict["sptresponsecode"] = 500
+                print(e)
+            infoDict["spiresponsecode"] = 200
+            print(" Success! Next refresh in "+str(reftime)+" seconds.")
+            loopCount=0
+        
+        loopCount+=1
+        time.sleep(1)
+    runningServiceCount-=1
 
 # --- Spotify Loop ---
 
 def spotifyloop():
-    while True:
+    runningServiceCount+=1
+    while (not isShutingDown):
         tmpname = str(infoDict["name"])
         tmpartists = str(infoDict["artists"])
         tmpisPlaying = bool(infoDict["isPlaying"])
@@ -169,28 +182,35 @@ def spotifyloop():
         # if (tmpisPlaying != infoDict["isPlaying"]):
         #     print(infoDict["isPlaying"])
         time.sleep(0.5)
+    runningServiceCount-=1
 
 # --- OpenWeather Fetcher Loop ---
 
 def weatherloop():
-    while True:
-        print("Getting Weather in "+city+"...", end="")
-        try:
-            observation = mgr.weather_at_place(city)
-            w = observation.weather
-            infoDict["temperature"] = round(w.temperature('celsius')['temp'])
-            infoDict["status"] = w.detailed_status.title()
-            infoDict["wtresponsecode"] = 200
-            print(" Success!")
-        except Exception as e:
-            infoDict["wtresponsecode"] = 500
-            print(e)
+    runningServiceCount+=1
+    loopCount = 0
+    while (not isShutingDown):
+        if loopCount >= 60*10:
+            print("Getting Weather in "+city+"...", end="")
+            try:
+                observation = mgr.weather_at_place(city)
+                w = observation.weather
+                infoDict["temperature"] = round(w.temperature('celsius')['temp'])
+                infoDict["status"] = w.detailed_status.title()
+                infoDict["wtresponsecode"] = 200
+                print(" Success!")
+            except Exception as e:
+                infoDict["wtresponsecode"] = 500
+                print(e)
+            loopCount=0
 
         # # Print the temperature and status
         # print("Temperature: "+str(infoDict["temperature"])+"°C")
         # print("Status: "+str(infoDict["status"]))
         
-        time.sleep(60*10) # 10 minutes in seconds
+        loopCount+=1
+        time.sleep(1)
+    runningServiceCount-=1
         
 # --- Starting threads ---
 
@@ -268,27 +288,28 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         
 
 def infoServer():
+    runningServiceCount+=1
     PORT = 18724
+    while (not isShutingDown):
+        try:
+            # Create a SSL context
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            
+            # Load the SSL certificate and key
+            context.load_cert_chain(certfile=path+'certificate.pem', keyfile=path+'key.pem', password=keyPass)
 
-    # Create a SSL context
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    
-    # Load the SSL certificate and key
-    context.load_cert_chain(certfile=path+'certificate.pem', keyfile=path+'key.pem', password=keyPass)
+            # Create the server and handler objects
+            server = socketserver.TCPServer(("", PORT), RequestHandler)
 
-    # Create the server and handler objects
-    server = socketserver.TCPServer(("", PORT), RequestHandler)
+            # Wrap the server in the SSL context
+            server.socket = context.wrap_socket(server.socket, server_side=True)
 
-    # Wrap the server in the SSL context
-    server.socket = context.wrap_socket(server.socket, server_side=True)
+            print("serving at port", PORT, end="")
+            print("... Success!")
 
-    print("serving at port", PORT, end="")
-    print("... Success!")
-    # Start the server loop
-    try:
-        server.serve_forever()
-    except Exception as e:
-        print("An error occurred: ", e)
+        except Exception as e:
+            print("An error occurred: ", e)
+    runningServiceCount-=1
     
 time.sleep(0.5)
 
@@ -299,3 +320,14 @@ infoserverthread = threading.Thread(target=infoServer)
 
 # Start the thread
 infoserverthread.start()
+
+def sigterm_handler(_signo, _stack_frame):
+    global isShutingDown
+    # Perform cleanup tasks here
+    # ...
+    # Exit gracefully
+    isShutingDown=True
+    time.sleep(3)
+    sys.exit(runningServiceCount)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
